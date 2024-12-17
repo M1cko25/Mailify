fd032d673d02307aab81c30aad5dbcc2a49f1b07
M1cko25 <154508740+M1cko25@users.noreply.github.com>
2024-12-17 23:28:37 +0800

    some changes

--- notify.php
+++ notify.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('Asia/Manila');
 require 'vendor/autoload.php'; // Adjust the path if necessary
 
 use PHPMailer\PHPMailer\PHPMailer;
@@ -25,7 +26,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 $username = $row['username'];
                 $email = $row['email'];
                 $receive_date = date("Y-m-d");
-                $receive_time = date("H:i:s", time());
+                $receive_time = date("H:i:s");
                 $status = "Mail Received";
         
                 $stmt = $conn->prepare("INSERT INTO `mails`(`receive_date`, `receive_time`, `status`, `user_id`, `user_email`) VALUES (?, ?, ?, ?, ?)");
@@ -39,8 +40,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                             $mail->isSMTP();                                            // Send using SMTP
                             $mail->Host       = 'smtp.gmail.com';                     // Set the SMTP server
                             $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-                            $mail->Username   = 'lutiva.micojake.bscs2022@gmail.com';               // SMTP username
-                            $mail->Password   = 'ttil yutm tbrm qvqg';                  // SMTP password
+                            $mail->Username   = 'mailify748@gmail.com';               // SMTP username
+                            $mail->Password   = 'zjgc qbdv pmyy kqbe';                  // SMTP password
                             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption
                             $mail->Port       = 587;                                    // TCP port to connect to
                         
@@ -69,5 +70,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 echo "No user found with this mailbox ID";
             }
 }
-
 ?>
